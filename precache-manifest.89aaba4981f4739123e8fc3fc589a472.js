9ddc013b0c306bc994fb02e371c93402c642d4ee
IrynaZolkina <wzyz7557@hotmail.com>
2020-04-15 22:20:25 -0700

    Updates

---
+++ precache-manifest.89aaba4981f4739123e8fc3fc589a472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2beeb3baaa751df0a27fe345f0d473f",
+    "url": "/kapriz/index.html"
+  },
+  {
+    "revision": "629ff792482697182022",
+    "url": "/kapriz/static/css/main.e3cc5ae6.chunk.css"
+  },
+  {
+    "revision": "b823d2bdb3a205e75dd3",
+    "url": "/kapriz/static/js/2.2b042f4d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kapriz/static/js/2.2b042f4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "629ff792482697182022",
+    "url": "/kapriz/static/js/main.3fad264d.chunk.js"
+  },
+  {
+    "revision": "6c4388236fdf133cbc9e",
+    "url": "/kapriz/static/js/runtime-main.7ef64154.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/kapriz/static/media/crown.ed28accb.svg"
+  }
+]);
